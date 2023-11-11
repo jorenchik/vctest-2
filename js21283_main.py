b5a280b9462b1716da8b2bfeacdc24d28b218ca4
jorenchik <jorens.stekels@gmail.com>
2023-11-11 10:13:47 +0200

    initial commit

---
+++ js21283_main.py
@@ -0,0 +1,9 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from js21283_libadd import *
+from js21283_libmult import *
+
+a = 13
+b = 5
+print(plus(a, b))
+print(mult(a, b))
